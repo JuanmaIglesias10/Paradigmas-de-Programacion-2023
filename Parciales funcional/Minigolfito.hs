6e7079556947926ea452a47fc8e7a2fb044a8f00
Juanmaiglesias10 <juanmaiglesiascasla@gmail.com>
2023-05-27 17:29:56 -0300

    Subo modelo de parcial de clase terminado

---
+++ Parciales funcional/Minigolfito.hs
@@ -0,0 +1,36 @@
+module Ejemplo where
+import Text.Show.Functions ()
+
+data Jugador = UnJugador {
+  nombre :: String,
+  padre :: String,
+  habilidad :: Habilidad
+} deriving (Eq, Show)
+
+data Habilidad = Habilidad {
+  fuerzaJugador :: Int,
+  precisionJugador :: Int
+} deriving (Eq, Show)
+
+bart :: Jugador
+bart = UnJugador "Bart" "Homero" (Habilidad 25 60)
+todd :: Jugador
+todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
+rafa :: Jugador
+rafa = UnJugador "Rafa" "Gorgory" (Habilidad 10 1)
+
+data Tiro = UnTiro {
+  velocidad :: Int,
+  precision :: Int,
+  altura :: Int
+} deriving (Eq, Show)
+
+type Puntos = Int
+
+
+--PUNTO 1 
+
+--A
+
+
+
--- Parciales funcional/ParcialSimulacro.hs
+++ Parciales funcional/ParcialSimulacro.hs
@@ -1,6 +1,5 @@
 module Ejemplo where
 import Text.Show.Functions ()
-import GHC.IO (unsafeUnmask)
 
 data Participante = UnParticipante {
     nombre       :: String,
@@ -15,7 +14,8 @@ data Plato = UnPlato {
     componentes :: [Componente]
 }deriving (Show,Eq)
 
-type Componente = (String,Int)
+type Componente = (Ingrediente,Int)
+type Ingrediente = String
 
 
 endulzar :: Int -> Truco
@@ -52,3 +52,66 @@ esUnBardo unPlato = dificultad unPlato > 7 && cantidadDeComponentes unPlato > 5
 
 cantidadDeComponentes :: Plato -> Int
 cantidadDeComponentes unPlato = length . componentes $ unPlato
+
+esVegano :: Plato -> Bool
+esVegano unPlato = not . any esProductoAnimal . componentes $ unPlato
+
+esProductoAnimal :: Componente -> Bool
+esProductoAnimal (ingrediente , _) = elem ingrediente productosAnimales
+
+productosAnimales :: [Ingrediente]
+productosAnimales = ["Leche", "Carne", "Huevo", "Manteca"]
+
+esSinTacc :: Plato -> Bool
+esSinTacc unPlato = not . any esProductoConHarina . componentes $ unPlato
+
+esProductoConHarina :: Componente -> Bool
+esProductoConHarina (ingrediente , _) = elem ingrediente productosConHarina
+
+productosConHarina :: [Ingrediente]
+productosConHarina = ["Pastas","Pizza","Pan"]
+
+esComplejo :: Plato -> Bool
+esComplejo unPlato = esUnBardo unPlato
+
+noAptoHipertension :: Plato -> Bool
+noAptoHipertension unPlato = any tieneMuchaSal (componentes  unPlato)
+
+tieneMuchaSal :: Componente -> Bool
+tieneMuchaSal unComponente = fst unComponente == "Sal" && snd unComponente > 2
+
+--PARTE B
+
+pepeRonccino :: Participante
+pepeRonccino = UnParticipante "Pepe Ronccino" [darSabor 2 5 , simplificar , duplicarPorcion] unPlatoComplejo
+
+unPlatoComplejo :: Plato
+unPlatoComplejo = UnPlato 10 [("Sal",5),("azucar",1),("champinion",4),("carne",2),("huevo",2),("lechuga",6)]
+
+--PARTE C 
+
+cocinar :: Participante -> Plato
+cocinar unParticipante = foldl aplicarTrucos (especialidad unParticipante) (trucos unParticipante) 
+
+aplicarTrucos :: Plato -> Truco -> Plato
+aplicarTrucos unPlato unTruco = unTruco unPlato
+
+esMejorQue :: Plato -> Plato -> Bool
+esMejorQue unPlato otroPlato = dificultad unPlato > dificultad otroPlato && pesoDelPlato unPlato < pesoDelPlato otroPlato
+
+pesoDelPlato :: Plato -> Int
+pesoDelPlato unPlato = sum . map (snd) . componentes $ unPlato
+
+participanteEstrella :: [Participante] -> Participante
+participanteEstrella unaListaDeParticipantes = foldl1 tieneElMejorPlato unaListaDeParticipantes
+
+tieneElMejorPlato :: Participante -> Participante -> Participante
+tieneElMejorPlato unParticipante otroParticipante
+    | esMejorQue (cocinar unParticipante) (cocinar otroParticipante) = unParticipante
+    | otherwise = otroParticipante
+
+-- Parte D
+
+platinum :: Plato
+platinum = UnPlato 10 (repeat ("carne",100))
+
