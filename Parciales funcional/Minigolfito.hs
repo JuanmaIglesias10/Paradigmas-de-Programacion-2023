ce653975bad77b014d088d3bd582fb9f7f22d8d8
Juanmaiglesias10 <juanmaiglesiascasla@gmail.com>
2023-05-27 19:37:53 -0300

    Subo punto 2 parcial Minigolfito

--- Parciales funcional/Minigolfito.hs
+++ Parciales funcional/Minigolfito.hs
@@ -32,5 +32,27 @@ type Puntos = Int
 
 --A
 
+type Palo = Habilidad -> Tiro 
+type Precision = Int
 
+putter :: Palo
+putter unaHabilidad = UnTiro {velocidad = 10 , precision = precisionJugador unaHabilidad * 2  , altura = 0}  
+
+madera :: Palo
+madera unaHabilidad = UnTiro {velocidad = 100 , precision = div (precisionJugador unaHabilidad) 2 , altura = 5}
+
+hierro :: Int -> Palo
+hierro unN unaHabilidad = UnTiro {velocidad =  fuerzaJugador unaHabilidad * unN , precision = precisionJugador unaHabilidad `div` unN , altura =  (unN - 3) `max` 0 } 
+
+--B
+
+palos :: [Palo]
+palos = [putter , madera] ++ map hierro [1..10]
+
+--PUNTO 2
+
+golpe :: Jugador -> Palo -> Tiro
+golpe unJugador unPalo = unPalo . habilidad $ unJugador
+
+--PUNTO 3
 
